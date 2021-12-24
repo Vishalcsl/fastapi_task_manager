9b840446fa64a711f5d431bce69b42bfc315c893
Vishalcsl <vishalgautamcsl@gmail.com>
2021-12-24 23:31:48 +0530

    abc

--- backend/main.py
+++ backend/main.py
@@ -7,6 +7,8 @@ from backend.core.config import settings
 from backend.apis.base import api_router
 from backend.db.session import engine
 from backend.db.base import Base
+from backend.db.models.tasks import Task
+from backend.db.models.users import User
 from backend.webapps.base import api_router as web_app_router
 
 # print(os.path.dirname(__file__))
@@ -20,7 +22,7 @@ def configure_static(app):
     app.mount("/backend/static", StaticFiles(directory="backend/static"), name="static")
 
 def create_tables():
-    if not engine.dialect.has_table(engine, "TASK" ) and not engine.dialect.has_table(engine, "USER" ): 
+    if not engine.dialect.has_table(engine, Task) and not engine.dialect.has_table(engine, User ): 
         Base.metadata.create_all(bind=engine)
 
 def start_application():
